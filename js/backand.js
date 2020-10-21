8d77de584616179a9dfb661073e404a19f2aec81
Konstantin <you@example.com>
2020-10-21 20:01:19 +0300

    Make initial commit

---
+++ js/backand.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const form = document.querySelector('.order__form');
+
+const formData = new FormData(form);
+
+console.log(formData);
+
+
+
+// TODO
+// 1. Посмотри как работать с FormData
+// 2. Бери выбранные поля ( см. index.html ) и сформируй заказ
+// 3. Разберись как отправить сформированный отчет на почту
